505e53ef3907f6fd931aedb6997c8ff57c00bda2
Tevy Jacobs-Gomes <tevydavid@gmail.com>
2015-12-15 11:21:56 -0800

    typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
     user
   end
 
-  def ssword=(password)
+  def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
