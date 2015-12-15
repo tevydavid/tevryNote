52464d457e19f28998845c72cd73a63395f466ca
Tevy Jacobs-Gomes <tevydavid@gmail.com>
2015-12-15 11:17:34 -0800

    testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
     user
   end
 
-  def password=(password)
+  def ssword=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
